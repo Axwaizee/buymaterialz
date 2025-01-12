35c24abc38bffc39da5d37009e2bf14626d43f38
Axwaizee <83546692+Axwaizee@users.noreply.github.com>
2025-01-12 19:39:59 +0530

    added images

--- src/components/AutoStartSlider.jsx
+++ src/components/AutoStartSlider.jsx
@@ -12,16 +12,16 @@ function AutoStartSlider() {
         <Carousel options={OPTIONS} isAutoPlay={true} className="w-full">
           <SliderContainer>
             <Slider className="w-full">
-              <div className="bg-red-500 h-[90dvh] w-full"></div>{' '}
+              <div className="h-[90dvh] w-full bg-cover bg-[url('./images/c1.jpg')]"></div>{' '}
             </Slider>
             <Slider className="w-full">
-              <div className="bg-blue-500 h-[90dvh] w-full"></div>{' '}
+              <div className="h-[90dvh] w-full bg-cover bg-[url('./images/c2.jpg')]"></div>{' '}
             </Slider>
             <Slider className="w-full">
-              <div className="bg-green-500 h-[90dvh] w-full"></div>{' '}
+              <div className="h-[90dvh] w-full bg-cover bg-[url('./images/c3.jpg')]"></div>{' '}
             </Slider>
             <Slider className="w-full">
-              <div className="bg-yellow-500 h-[90dvh] w-full"></div>{' '}
+              <div className="h-[90dvh] w-full bg-cover bg-[url('./images/c4.jpg')]"></div>{' '}
             </Slider>
           </SliderContainer>
         </Carousel>
